bcdb03a48f5b232520ccb1042c92fc5cf14ab99c
Madhurima Khamroy <madhurimakhamroy@gmail.com>
2025-09-04 17:26:06 +0200

    Updated schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, Field
+from typing import Annotated
 
+# pydantic model to validate input features for prediction
 class PersonalityFeatures(BaseModel):
-    Time_spent_Alone: str
-    Stage_fear: str
-    Social_event_attendance: str
-    Going_outside: str
-    Drained_after_socializing: str
-    Friends_circle_size: str
-    Post_frequency: str
+    Time_spent_Alone: Annotated[float, Field(ge=0, le=11, description="Hours spent alone per day")]
+    Stage_fear: Annotated[str, Field(description="Fear of public speaking (Yes/No)")]
+    Social_event_attendance: Annotated[float, Field(ge=0, le=10, description="Frequency of attending social events")]
+    Going_outside: Annotated[float, Field(ge=0, le=7, description="Frequency of going outside")]
+    Drained_after_socializing: Annotated[str, Field(description="Feeling drained after social interactions (Yes/No)")]
+    Friends_circle_size: Annotated[float, Field(ge=0, le=15, description="Size of friends circle")]
+    Post_frequency: Annotated[float, Field(ge=0, le=10, description="Frequency of posting on social media")]
